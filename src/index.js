8a81f26ac462e221ab1e8b415939ec42cda4ed62
prudence <prudence.jerono@student.moringaschool.com>
2023-04-19 08:18:51 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -3,3 +3,15 @@ import ReactDOM from "react-dom";
 import ColoradoStateParks from "./ColoradoStateParks";
 
 ReactDOM.render(<ColoradoStateParks />, document.getElementById("root"));
+
+
+test("Test passing", () => {
+    return new Promise((resolve, reject) => {
+      resolve(true);
+    });
+  }).then(result => {
+    console.log("Test passed!");
+  }).catch(error => {
+    console.error("Test failed:", error);
+  });
+  
